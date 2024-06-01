ef9a684da4c936cbc7b6f4e98dc4f6b22f6c1a46
chaeyoungg <chaeyoungg77@gmail.com>
2024-06-01 21:38:18 +0900

    Design : 사이드바 디자인 변경

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,14 +1,24 @@
+import { Link, Outlet } from 'react-router-dom';
 import './Sidebar.css';
 
 function Sidebar() {
   return (
-    <div className="side-wrapper">
-      <ul className="side-list">
-        <li className="list-items">ai - 1</li>
-        <li className="list-items">ai - 2</li>
-        <li className="list-items">ai - 3</li>
-        <li className="list-items">ai - 4</li>
-      </ul>
+    <div className="wrapper">
+      <div className="l-wrapper">
+        <div className="side-wrapper">
+          <Link className="side-title">
+            <img className="icon-image" src="/icon.svg" alt="" />
+            <h2 className="icon-title">Ai 문장해석</h2>
+          </Link>
+          <ul className="side-list">
+            <li className="list-items">긴 글에서 정보 추출하기AI</li>
+            <li className="list-items">ai - 2</li>
+            <li className="list-items">ai - 3</li>
+            <li className="list-items">ai - 4</li>
+          </ul>
+        </div>
+        <Outlet />
+      </div>
     </div>
   );
 }
