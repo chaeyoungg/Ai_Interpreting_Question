78dc115e2a3d2af6b588bcd82a2ba2cc13ed51b7
chaeyoungg <chaeyoungg77@gmail.com>
2024-06-08 00:22:19 +0900

    Design : 사이드바, 컨텐츠 디자인 정리

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -2,7 +2,6 @@ import useAxiosAi from '@hooks/useAxiosAi.mjs';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import './Question.css';
-import Sidebar from '@layout/Sidebar';
 
 function Question() {
   const { register, handleSubmit } = useForm();
@@ -28,7 +27,13 @@ function Question() {
     <div className="contents-wrap">
       <h1 className="contents-title">Ai_Interpreting_Question</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <label className="passage-label" htmlFor="passage">
+          [ 긴 글 ]
+        </label>
+        <br />
+
         <textarea
+          className="passage"
           id="passage"
           defaultValue="루트비히 판 베토벤(독일어: Ludwig van Beethoven, 문화어: 루드위히 판 베토벤, 1770년 12월 17일 ~ 1827년 3월 26일)은 독일의 서양 고전 음악 작곡가이다. 독일의 본에서 태어났으며, 성인이 된 이후 거의 오스트리아 빈에서 살았다. 감기와 폐렴으로 인한 합병증으로 투병하다가 57세로 생을 마친 그는 고전주의와 낭만주의의 전환기에 활동한 주요 음악가이며, 작곡가로 널리 존경받고 있다. 음악의 성인(聖人) 또는 악성(樂聖)이라는 별칭으로 부르기도 한다. 가장 잘 알려진 작품 가운데에는 〈교향곡 5번〉, 〈교향곡 6번〉, 〈교향곡 9번〉, 〈비창 소나타〉, 〈월광 소나타〉 등이 있다."
           {...register('argument.passage', {
@@ -36,8 +41,13 @@ function Question() {
           })}
         />
         <br />
+        <label className="passage-label" htmlFor="question">
+          [ 얻고 싶은 정보 ]
+        </label>
+        <br />
         <input
           type="text"
+          className="question"
           id="question"
           defaultValue="베토벤의 출생일"
           {...register('argument.question', {
@@ -45,11 +55,13 @@ function Question() {
           })}
         />
 
-        <button type="submit">해석하기</button>
+        <button className="submit" type="submit">
+          해석하기
+        </button>
 
-        <h2>결과 보기</h2>
-        <p>{result}</p>
-        {/* <textarea id="result" value={result}></textarea> */}
+        <hr className="content-hr" />
+        <h3 className="result-label">[ 결과 보기 ]</h3>
+        <p className="result">{result}</p>
       </form>
     </div>
     //   </div>
