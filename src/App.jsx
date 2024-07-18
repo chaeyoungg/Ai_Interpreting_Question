af4e2902382a297f6c4c478f96773633d9ae231d
chaeyoungg <chaeyoungg77@gmail.com>
2024-07-18 18:13:21 +0900

    Chore : 배포를 위한 깃허브 페이지 패키지 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Question from '@pages/Question';
 import './App.css';
-import Sidebar from '@layout/Sidebar';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
